169f9d1faf4858bb9b829398dee61c4de092ec1c
MOHD ADIL <adilrais2001@gmail.com>
2020-07-21 17:55:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+alert("Touch the cake 🎂 4 times and enjoy the party 🎶🎵🎉🎊");
+
+window.addEventListener('click', function () {
+    
+    var audio = document.getElementById("HBD");
+    audio.play();
+    
+});
